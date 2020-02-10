f6fa6040345b7f5afde3d6e3ca179163cbbcd722
Malwina8 <48411653+Malwina8@users.noreply.github.com>
2020-02-10 21:16:24 +0100

    Add new actions to filterRedux.js

--- src/redux/filtersRedux.js
+++ src/redux/filtersRedux.js
@@ -10,11 +10,15 @@ const createActionName = name => `app/${reducerName}/${name}`;
 
 // action types
 export const CHANGE_PHRASE = createActionName('CHANGE_PHRASE');
-// TODO - add other action types
+export const CHANGE_DURATION = createActionName('CHANGE_DURATION');
+export const CHANGE_TAGS = createActionName('CHANGE_TAGS');
+
 
 // action creators
 export const changeSearchPhrase = payload => ({ payload, type: CHANGE_PHRASE });
-// TODO - add other action creators
+export const changeDuration = payload => ({ payload, type: CHANGE_DURATION });
+export const changeTags = payload => ({ payload, type: CHANGE_TAGS });
+
 
 // reducer
 export default function reducer(statePart = [], action = {}) {
