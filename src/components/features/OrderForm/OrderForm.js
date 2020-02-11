3226251ac17d02a14e048fdde2dce1124ce92df3
Malwina8 <48411653+Malwina8@users.noreply.github.com>
2020-02-12 00:40:50 +0100

    Add OrderForm to Trip Component

---
+++ src/components/features/OrderForm/OrderForm.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+// import {Link} from 'react-router-dom';
+// import styles from './OrderForm.scss';
+// import SideImage from '../../common/SideImage/SideImage';
+// import DetailsBox from '../../common/DetailsBox/DetailsBox';
+// import DetailsImage from '../../common/DetailsImage/DetailsImage';
+// import List from '../../common/List/List';
+// import ListItem from '../../common/ListItem/ListItem';
+import {Row, Col} from 'react-flexbox-grid';
+
+const OrderForm = () => (
+
+
+  <Row>
+    <Col xs={12}>
+    </Col>
+  </Row>
+
+
+
+);
+
+OrderForm.propTypes = {
+
+  name: PropTypes.string,
+
+};
+
+export default OrderForm;
