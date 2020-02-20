5fe43fa232ab3001072d2d75b60e5a0ca76fa486
Malwina8 <48411653+Malwina8@users.noreply.github.com>
2020-02-20 01:47:18 +0100

    Add new Components & import to OrderOption

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -19,7 +19,6 @@ const OrderForm = props => (
 
 OrderForm.propTypes = {
   options: PropTypes.node,
-
   tripCost: PropTypes.string,
 }
 
--- src/components/features/OrderOption/OrderOption.js
+++ src/components/features/OrderOption/OrderOption.js
@@ -1,19 +1,34 @@
 import React from 'react'
 import styles from './OrderOption.scss'
+import OrderOptionDropdown from './OrderOptionDropdown'
+import OrderOptionIcons from './OrderOptionIcons'
+import OrderOptionNumber from './OrderOptionNumber'
+import OrderOptionCheckboxes from './OrderOptionCheckboxes'
+
+const optionTypes = {
+  dropdown: OrderOptionDropdown,
+  icons: OrderOptionIcons,
+  checkboxes: OrderOptionCheckboxes,
+  number: OrderOptionNumber,
+}
 
 
-class OrderOption extends React.Component {
-  state = {};
-
-  render() {
+const OrderOption = ({ name, type, ...otherProps }) => {
+  const OptionComponent = optionTypes[type]
+  if (!OptionComponent) {
+    return null
+  }
+  else {
     return (
       <div className={styles.component}>
         <h3 className={styles.title}>{name}</h3>
+        <OptionComponent
+          {...otherProps}
+        />
       </div>
     )
   }
 }
-
 
 
 export default OrderOption
