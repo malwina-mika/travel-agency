d474d136ed86705bd5b43c97540fd57a55b94676
Malwina8 <48411653+Malwina8@users.noreply.github.com>
2020-02-20 01:24:54 +0100

    Add Options from pricing.json to OrderForm

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -4,18 +4,22 @@ import OrderSummary from '../../features/OrderSummary/OrderSummary'
 import OrderOption from '../../features/OrderOption/OrderOption'
 
 import { Row, Col } from 'react-flexbox-grid'
+import pricing from '../../../data/pricing.json'
+
+
 
 const OrderForm = props => (
   <Row>
+    {pricing.map(option => (<Col md={4} key={option.id}> <OrderOption {...option} /></Col>))}
     <Col xs={12}>
       <OrderSummary cost={props.tripCost} options={props.options} />
-      <OrderOption />
     </Col>
   </Row>
 )
 
 OrderForm.propTypes = {
   options: PropTypes.node,
+
   tripCost: PropTypes.string,
 }
 
