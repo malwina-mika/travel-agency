25e348dad4d648c6e9072f3d5892e65c1f6b43ed
Malwina8 <48411653+Malwina8@users.noreply.github.com>
2020-02-21 00:03:55 +0100

    Add content to content to OrderOptionDropdown

---
+++ src/components/features/OrderOption/OrderOptionIcons.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const OrderOptionIcons = ({ name }) => {
+  return (
+    <div>
+      {name}
+    </div>
+  )
+}
+
+
+OrderOptionIcons.propTypes = {
+  name: PropTypes.string,
+}
+
+export default OrderOptionIcons  
---
+++ src/components/features/OrderOption/OrderOptionNumber.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const OrderOptionNumber = ({ name }) => {
+  return (
+    <div>
+      {name}
+    </div>
+  )
+}
+
+
+OrderOptionNumber.propTypes = {
+  name: PropTypes.string,
+}
+
+export default OrderOptionNumber   
