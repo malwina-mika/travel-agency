0c19d738f5d30b9be3b39ae59b6d92e7a1b3b9ee
Malwina8 <48411653+Malwina8@users.noreply.github.com>
2020-02-21 00:46:48 +0100

    Add content to content to OrderOptionIcons

--- src/components/features/OrderOption/OrderOptionIcons.js
+++ src/components/features/OrderOption/OrderOptionIcons.js
@@ -1,17 +1,34 @@
 import React from 'react'
+import styles from './OrderOption.scss'
 import PropTypes from 'prop-types'
+import { formatPrice } from '../../../utils/formatPrice'
+import Icon from '../../common/Icon/Icon'
 
-const OrderOptionIcons = ({ name }) => {
-  return (
-    <div>
-      {name}
-    </div>
-  )
-}
+const OrderOptionIcons = ({ values, required, currentValue, setOptionValue }) => (
+  <div
+    className={styles.icon}
+    value={currentValue}
+  >
+    {required === undefined || required === false ? '' : (
+      <div key='null' value=''><Icon name="times-circle" />none</div>
+    )}
+    {values.map(value => (
+      <div className={value.id === currentValue ? styles.iconActive : styles.icon}
+        key={value.id}
+        onChange={event => setOptionValue(event.currentTarget.value)}
+        value={value.id}><Icon name={value.icon} />
+        {value.name}
+        ({formatPrice(value.price)})</div>
+    ))}
 
+  </div>
+)
 
 OrderOptionIcons.propTypes = {
-  name: PropTypes.string,
+  required: PropTypes.bool,
+  values: PropTypes.array,
+  setOptionValue: PropTypes.func,
+  currentValue: PropTypes.string,
 }
 
-export default OrderOptionIcons  
+export default OrderOptionIcons 
