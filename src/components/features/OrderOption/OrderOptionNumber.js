504cf480755f5486371b4c810747244e87cdabdf
Malwina8 <48411653+Malwina8@users.noreply.github.com>
2020-02-21 01:13:29 +0100

    Add content to content to OrderOptionNumber

--- src/components/features/OrderOption/OrderOptionNumber.js
+++ src/components/features/OrderOption/OrderOptionNumber.js
@@ -1,17 +1,27 @@
 import React from 'react'
+import styles from './OrderOption.scss'
 import PropTypes from 'prop-types'
+import { formatPrice } from '../../../utils/formatPrice'
 
-const OrderOptionNumber = ({ name }) => {
-  return (
-    <div>
-      {name}
-    </div>
-  )
-}
 
+const OrderOptionNumber = ({ limits, currentValue, setOptionValue, price }) => (
+  <div
+    className={styles.number}
+  >
+    <input type='number' className={styles.inputSmall}
+      value={currentValue}
+      min={limits.min}
+      max={limits.max}
+      onChange={event => setOptionValue(event.currentTarget.value)} />
+    ({formatPrice(price)})
+  </div>
+)
 
 OrderOptionNumber.propTypes = {
-  name: PropTypes.string,
+  setOptionValue: PropTypes.func,
+  currentValue: PropTypes.string,
+  price: PropTypes.string,
+  limits: PropTypes.object,
 }
 
-export default OrderOptionNumber   
+export default OrderOptionNumber 
