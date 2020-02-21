ed4c8421b4a82e1998fba4d0277d865e4b3c3d73
Malwina8 <48411653+Malwina8@users.noreply.github.com>
2020-02-21 18:21:35 +0100

    Fix bug in OrderOptionIcons

--- src/components/features/OrderOption/OrderOptionIcons.js
+++ src/components/features/OrderOption/OrderOptionIcons.js
@@ -6,18 +6,21 @@ import Icon from '../../common/Icon/Icon'
 
 const OrderOptionIcons = ({ values, required, currentValue, setOptionValue }) => (
   <div
-    className={styles.icon}
-    value={currentValue}
-  >
-    {required === undefined || required === false ? '' : (
-      <div key='null' value=''><Icon name="times-circle" />none</div>
+    className={styles.icon}>
+    {required == undefined || required === 'false' ? '' : (
+      <div className={styles.icon}
+        value={currentValue}
+        onClick={() => setOptionValue('')}>
+        <Icon name="times-circle" />none</div>
     )}
     {values.map(value => (
       <div className={value.id === currentValue ? styles.iconActive : styles.icon}
         key={value.id}
-        onChange={event => setOptionValue(event.currentTarget.value)}
-        value={value.id}><Icon name={value.icon} />
+        value={value.id}
+        onClick={() => setOptionValue(value.id)}>
+        <Icon name={value.icon} />
         {value.name}
+
         ({formatPrice(value.price)})</div>
     ))}
 
