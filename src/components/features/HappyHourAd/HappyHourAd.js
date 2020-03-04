b460d36120607c94687fd88a5adb9c9ea2458411
Malwina8 <48411653+Malwina8@users.noreply.github.com>
2020-03-04 21:42:16 +0100

    Add HappyHourAd

---
+++ src/components/features/HappyHourAd/HappyHourAd.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styles from './HappyHourAd.scss'
+
+class HappyHourAd extends React.Component {
+  static propTypes = {
+    title: PropTypes.string,
+    promoDescription: PropTypes.string,
+  }
+
+  static defaultProps = {
+    title: 'Happy Hour',
+    promoDescription: 'It is your time! Take advantage of Happy Hour! All offers 20% off!',
+  }
+
+  constructor() {
+    super()
+
+    setInterval(() => { this.forceUpdate() }, 1000)
+  }
+
+  getCountdownTime() {
+    const currentTime = new Date()
+    const nextNoon = new Date(Date.UTC(currentTime.getUTCFullYear(), currentTime.getUTCMonth(), currentTime.getUTCDate(), 12, 0, 0, 0))
+
+    if (currentTime.getUTCHours() >= 12) {
+      nextNoon.setUTCDate(currentTime.getUTCDate() + 1)
+    }
+
+    return Math.round((nextNoon.getTime() - currentTime.getTime()) / 1000)
+  }
+
+  render() {
+
+    const { title, promoDescription } = this.props
+    const counter = this.getCountdownTime()
+
+    return (
+      <div className={styles.component}>
+        <h3 className={styles.title}>{title}</h3>
+        <div className={styles.promoDescription}>
+          {(counter > 23 * 60 * 60) ? promoDescription : counter}
+        </div>
+        <div className={styles.promoDescription}>{counter}</div>
+
+      </div>)
+  }
+}
+
+
+
+export default HappyHourAd
